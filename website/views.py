244e3a30a36e5e19e13f29ad93d1dbffe6f7ba4d
iamakhildabral <akhildabral02@gmail.com>
2023-05-26 02:18:17 +0530

    'registartion_loginandLogoutAdded'

--- website/views.py
+++ website/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login , logout
+from django.contrib import messages
 
 # Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        username_entered = request.POST['username']
+        password_entered = request.POST['password']
+
+        ## authenticating user
+        user = authenticate(request, username= username_entered, password= password_entered)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Welcome, You have been logged in!")
+            return redirect('home')
+        else:
+            messages.success(request,"There has been an error loggin in , Please try again ")
+            return redirect('home')
+    return render(request, 'home.html',{})
+
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You have been logged out...")
+    return redirect('home')
+
+def register_user(request):
+    return render(request,'register.html',{})
